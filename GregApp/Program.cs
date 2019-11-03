d63e89da05597882fb9602069823aad92deeca0a
Joe Rae <josephrae@gmail.com>
2019-11-02 22:11:27 -0400

    Initial Commit

---
+++ GregApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace GregApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                String pathName = (x + ".txt");
+                String fileName = "C:\\Users\\Slodysko\\Dropbox\\Remote_Control\\" + pathName;
+
+                if (File.Exists(fileName))
+                {
+                    // execute exe
+
+                    String exeFile = "F:\\nutcracker-xlights\\Sequences\\Shows\\AutoIt Exec files\\" + pathName;
+
+                    Process.Start(exeFile);
+
+                    // wait two seconds
+
+                    Thread.Sleep(2000);
+
+                    // delete file
+                    File.Delete(fileName);
+                }
+            }
+
+        }
+    }
+}
